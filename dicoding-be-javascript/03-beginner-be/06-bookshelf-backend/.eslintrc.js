8442e8b324e00d6468ae8bbf98e870607d5ab956
Agus SM <mrschwartx@gmail.com>
2025-08-28 14:50:13 +0700

    feat(dicoding-be-javascript): update course

---
+++ dicoding-be-javascript/03-beginner-be/06-bookshelf-backend/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    'env': {
+      'browser': true,
+      'commonjs': false,
+      'es2021': true,
+    },
+    'extends': 'google',
+    'overrides': [
+    ],
+    'parserOptions': {
+      'ecmaVersion': 'latest',
+    },
+    'rules': {
+      'linebreak-style': 'off',
+      'space-before-blocks': 'off',
+      'require-jsdoc': 'off',
+      'padded-blocks': 'off',
+      'indent': 'off',
+      'max-len': 'off',
+      'no-trailing-spaces': 'off',
+    },
+  };
+  
