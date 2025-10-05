5f71584efe8080302d9afdf2f02c8e3ac0b379a4
Agus Syahril Mubarok <mrschwartx@gmail.com>
2025-10-05 10:05:02 -0500

    feat(fc-msa-promotions): add swagger

--- fc-msa-promotions/user-service/build.gradle.kts
+++ fc-msa-promotions/user-service/build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("com.auth0:java-jwt:4.4.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
     runtimeOnly("org.postgresql:postgresql")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
--- fc-msa-promotions/user-service/src/main/java/com/example/user/config/SwaggerConfig.java
+++ fc-msa-promotions/user-service/src/main/java/com/example/user/config/SwaggerConfig.java
@@ -1,4 +1,43 @@
 package com.example.user.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.*;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openApiSpec() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Service API in Promotions")
+                        .version("1.0.0"))
+                .components(new Components()
+                        .addSchemas("ApiErrorResponse", new ObjectSchema()
+                                .addProperty("status", new IntegerSchema())
+                                .addProperty("code", new StringSchema())
+                                .addProperty("message", new StringSchema())
+                                .addProperty("fieldErrors", new ArraySchema().items(
+                                        new Schema<ArraySchema>().$ref("ApiFieldError"))))
+                        .addSchemas("ApiFieldError", new ObjectSchema()
+                                .addProperty("code", new StringSchema())
+                                .addProperty("message", new StringSchema())
+                                .addProperty("property", new StringSchema())
+                                .addProperty("rejectedValue", new ObjectSchema())
+                                .addProperty("path", new StringSchema()))
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")));
+    }
 }
+
