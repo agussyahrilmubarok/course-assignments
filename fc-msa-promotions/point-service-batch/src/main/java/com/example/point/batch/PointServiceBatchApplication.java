35e062b22509771904d8824cbf8c10f1d908e90d
Agus Syahril Mubarok <mrschwartx@gmail.com>
2025-10-10 23:45:33 -0500

    feat(fc-msa-promotions): setup point batch service

---
+++ fc-msa-promotions/point-service-batch/src/main/java/com/example/point/batch/PointServiceBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.point.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointServiceBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointServiceBatchApplication.class, args);
+	}
+
+}
---
+++ fc-msa-promotions/point-service-batch/src/test/java/com/example/point/batch/PointServiceBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.point.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointServiceBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
