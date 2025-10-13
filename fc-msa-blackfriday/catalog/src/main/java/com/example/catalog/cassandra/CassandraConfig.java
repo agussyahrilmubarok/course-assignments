8b1e1f39d74ed81876feece612be356efa07dbdd
Agus Syahril Mubarok <mrschwartx@gmail.com>
2025-10-13 17:08:05 +0700

    Update CassandraConfig.java

--- fc-msa-blackfriday/catalog/src/main/java/com/example/catalog/cassandra/CassandraConfig.java
+++ fc-msa-blackfriday/catalog/src/main/java/com/example/catalog/cassandra/CassandraConfig.java
@@ -1,4 +1,23 @@
 package com.example.catalog.cassandra;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.core.CassandraAdminTemplate;
+import org.springframework.data.cassandra.core.cql.CqlTemplate;
+
+@Configuration
 public class CassandraConfig {
+
+    private static final String KEYSPACE_NAME = "catalog";
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Boolean createKeyspaceIfNotExists(CqlTemplate cqlTemplate) {
+        String createKeyspace =
+            "CREATE KEYSPACE IF NOT EXISTS " + KEYSPACE_NAME + " " +
+            "WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}";
+        cqlTemplate.execute(createKeyspace);
+        return true; // just to ensure bean creation
+    }
 }
