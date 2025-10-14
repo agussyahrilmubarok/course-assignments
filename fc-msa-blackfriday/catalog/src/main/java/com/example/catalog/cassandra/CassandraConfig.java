f246ed729814ba38484c389f2957a76c7ef7f697
Agus Syahril Mubarok <mrschwartx@gmail.com>
2025-10-13 20:59:37 -0500

    fix(fc-msa-blackfriday): auto migrate schema on cassandra

--- fc-msa-blackfriday/catalog/src/main/java/com/example/catalog/cassandra/CassandraConfig.java
+++ fc-msa-blackfriday/catalog/src/main/java/com/example/catalog/cassandra/CassandraConfig.java
@@ -1,23 +1,12 @@
 package com.example.catalog.cassandra;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.cassandra.core.CassandraAdminTemplate;
-import org.springframework.data.cassandra.core.cql.CqlTemplate;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @Configuration
+@EnableCassandraRepositories("com.example.catalog.cassandra.repos")
+@EntityScan("com.example.catalog.cassandra.domain")
 public class CassandraConfig {
 
-    private static final String KEYSPACE_NAME = "catalog";
-
-    @Bean
-    @ConditionalOnMissingBean
-    public Boolean createKeyspaceIfNotExists(CqlTemplate cqlTemplate) {
-        String createKeyspace =
-            "CREATE KEYSPACE IF NOT EXISTS " + KEYSPACE_NAME + " " +
-            "WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}";
-        cqlTemplate.execute(createKeyspace);
-        return true; // just to ensure bean creation
-    }
 }
