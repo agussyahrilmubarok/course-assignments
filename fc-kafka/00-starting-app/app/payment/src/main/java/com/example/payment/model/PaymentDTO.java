0272af70cc6736ba1461af7b39dc9576809e1d97
Agus Syahril Mubarok <mrschwartx@gmail.com>
2025-10-19 06:02:18 -0500

    feat(fc-kafka): update course

---
+++ fc-kafka/00-starting-app/app/payment/src/main/java/com/example/payment/model/PaymentDTO.java
@@ -0,0 +1,46 @@
+package com.example.payment.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+@Getter
+@Setter
+public class PaymentDTO {
+
+    private UUID id;
+
+    @NotNull
+    @Size(max = 255)
+    private String orderId;
+
+    @NotNull
+    @Size(max = 255)
+    private String customerId;
+
+    @NotNull
+    @Digits(integer = 10, fraction = 2)
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    @Schema(type = "string", example = "92.08")
+    private BigDecimal amount;
+
+    @Size(max = 255)
+    private String method;
+
+    @NotNull
+    @Size(max = 255)
+    private String status;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
+    private OffsetDateTime paidAt;
+
+}
